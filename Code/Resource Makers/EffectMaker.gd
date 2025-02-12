d1c1fb6f8342f8209241df8d91cbdb472b995228
Jose-YP <joseaw2003@gmail.com>
2025-02-12 11:41:30 -0500

    Making attack displays

--- Code/Resource Makers/EffectMaker.gd
+++ Code/Resource Makers/EffectMaker.gd
@@ -12,15 +12,23 @@ class_name EffectCall
 @export_group("Disable")
 @export var instead_of_damage: bool = false
 @export var do_nothing: bool = false
-@export var retreat: bool = false
 @export_enum("Can", "Flip", "Can't") var attack: int = 0
+@export_subgroup("Retrat")
+##-1 means forever, otherwise it's turn duration
+@export var retrat_duration: int = 1
+@export var retreat: bool = false
+
 
 @export_group("Buff & Debuff")
+##Who recieves the buff?
 @export_enum("PlayerAll", "OpponentAll", "Self",
-"Defender", "Both Active", "Both Defending") var buff_target: int = 0 ##Who recieves the buff?
-@export_range(-120, 120, 10) var attack_damage: int = 0 ##How much more damage to the active spot?
-@export_range(-120, 120, 10) var defense: int = 0 ##How much less damage should be taken from opponent
-@export var after_weak_res: bool = true ##Is it applied before or after weak/res
+"Defender", "Both Active", "Both Defending") var buff_target: int = 0
+##How much more damage to the active spot?
+@export_range(-120, 120, 10) var attack_damage: int = 0
+##How much less damage should be taken from opponent
+@export_range(-120, 120, 10) var defense: int = 0
+##Is it applied before or after weak/res
+@export var after_weak_res: bool = true
 
 @export_group("Utility")
 @export_enum("None", "P by P", "O by O",
@@ -31,11 +39,16 @@ class_name EffectCall
 @export_enum("Player", "Opponent") var disrupt_target: int = 0
 @export_enum("None","Hand", "Deck") var disrupting: int = 0
 @export var card_ammount: int = 0
+@export var reveal: bool = false
 @export_subgroup("Energy")
 @export var energy_ammount: int = 0
 
 @export_group("Other")
 @export var healing: HealEffect
-#mimicry might be it's own resource
+##mimicry might be it's own resource
 @export_enum("No","AttatchedLimited", "Attatched",
  "AnyLimited", "Any") var mimic: int = 0
+##Ask for any extra effects
+@export var ask_extra: Ask
+##Do extra effect for extra ask
+@export var extra_effect: EffectCall
--- Scenes/Testing/Debug_slots.gd
+++ Scenes/Testing/Debug_slots.gd
@@ -8,8 +8,13 @@ const energy_cards: Array[String] = ["res://Cards/1 ex Ruby & Saphire/RS104Grass
 "res://Cards/1 ex Ruby & Saphire/RS95RainbowEnergy.tres"]
 const conditions: Array[String] = ["Poison", "Burn", "Asleep", 
 "Paralyze", "Confusion", "Shockwave", "Imprison"]
+const evolutions: Array[String] = ["res://Cards/4 ex Team Magma VS Team Aqua/MA37MagmaMightyena.tres",
+"res://Cards/1 ex Ruby & Saphire/RS42Mightyena.tres","res://Cards/1 ex Ruby & Saphire/RS37Lairon.tres",
+"res://Cards/1 ex Ruby & Saphire/RS29Delcatty.tres","res://Cards/1 ex Ruby & Saphire/RS33Hariyama.tres",
+"res://Cards/1 ex Ruby & Saphire/RS1Aggron.tres"]
 
 var energy_type: int = 0
+var evo_type: int = 0
 var condition_type: int = 0
 @onready var slots: Array[PokeSlot] = [$PokeSlot, $PokeSlot2]
 
@@ -18,6 +23,7 @@ func _ready():
 	slots[0].refresh()
 	slots[1].refresh()
 
+#region ENERGY
 func _on_energy_options_item_selected(index: int):
 	energy_type = index
 	print(energy_cards[index])
@@ -25,24 +31,43 @@ func _on_energy_options_item_selected(index: int):
 	print(card.name, card.energy_properties.type)
 	print(card.energy_properties.how_display())
 
-func add_energy_to(target: int):
+func add_energy_to(_target: int):
 	pass # Replace with function body.
 
-func remove_energy_from(target: int):
+func remove_energy_from(_target: int):
 	pass
+#endregion
 
+#region EVOLUTION
+func _on_evolution_options_item_selected(index):
+	evo_type = index
+	var card: Base_Card = load(evolutions[evo_type])
+	
+	print(card.name," evolves from: ", card.pokemon_properties.evolves_from)
+	
+	for slot in slots:
+		print("Can ",slot.current_card.name," evolve? ",slot.current_card.name == card.pokemon_properties.evolves_from)
+	
+
+func _on_evolve_pressed(_target: int):
+	pass # Replace with function body.
+
+#endregion
+
+#region CONDITION
 func change_conditiion(index: int):
 	condition_type = index
 
 func add_condition():
 	pass
 
-func add_imprision_to(target: int):
+func add_imprision_to(_target: int):
 	pass
 
-func add_shockwave_to(target: int):
+func add_shockwave_to(_target: int):
 	pass
 
-func clear_conditions_from(target: int):
+func clear_conditions_from(_target: int):
 	pass
+#endregion
 
