9aac2a3da8b1db9895250b2535e897329ef56ce7
Jose-YP <joseaw2003@gmail.com>
2025-02-23 16:56:11 -0500

    I think every current card is fixed

--- Code/Resource Makers/EffectSpecific/EnMovMaker.gd
+++ Code/Resource Makers/EffectSpecific/EnMovMaker.gd
@@ -1,15 +1,21 @@
 extends Resource
 class_name EnMov
 
-@export_group("Energy Movement")
+#@export_group("Energy Movement")
 @export_enum("Basic", "Special", "Any") var energy_move_type: int = 0
-@export_enum("Move", "Discard") var action: int = 1
+@export_enum("Move", "Discard", 
+"Top Deck", "Bottom Deck") var action: int = 1
+##If they targets from which meet ask, they're allowed
+@export var candidates: Ask
 ##Targets for removal
 @export_flags("Attacker", "Defender", "OtherActive",
 "Other Defending", "PBench", "OBench", "All") var which: int = 1
+##Ammount of energy per swap. -1 means infinite.
 @export var energy_ammount: int = 0
+##Ammount of swaps allowed. -1 means infinite.
+@export var swap_ammount: int = 1
 ##If any energy is currently considered
 @export_flags("Grass","Fire","Water",
 "Lightning","Psychic","Fighting",
-"Darkness","Metal","Colorless") var type: int = 0
+"Darkness","Metal","Colorless") var type: int = 2 ** 9 - 1
 
--- Code/Resource Makers/PokemonSpecific/BenchAttackMaker.gd
+++ Code/Resource Makers/PokemonSpecific/BenchAttackMaker.gd
@@ -1,9 +1,12 @@
 extends Resource
 class_name BenchAttk
 
-@export_group("Damage")
 @export_range(-1,5) var how_many: int = 1
+@export var candidates: Ask
 @export_enum("Player","Opponent","All") var which: int = 1
+
+@export_group("Damage")
+@export var apply_weak_res: bool = false
 @export_range(0,200,10) var initial_main_DMG: int = 0
 @export_enum("None", "Add", "Multiply", "Subtract") var modifier: int = 0
 @export var counter: Counter
