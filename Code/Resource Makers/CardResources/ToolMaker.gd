4ac5e6c0c1546a70df6d96529ca537e35112c317
Jose-YP <joseaw2003@gmail.com>
2025-06-10 11:30:04 -0400

    No more repairs for now

--- Code/Resource Makers/CardResources/ToolMaker.gd
+++ Code/Resource Makers/CardResources/ToolMaker.gd
@@ -2,6 +2,7 @@ extends Resource
 class_name ToolCard
 
 @export_enum("Anytime", "Between Turns") var when_to_check: int = 0
+@export var prompt_ask: PromptAsk
 ##Is there anything that needs to happen for the tool to activate?
 @export var activate_ask: SlotAsk
 @export var tool_effect: EffectCall
