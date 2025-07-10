56951e788299041f3ae96a38858f0f103a06cc2f
Jose <joseaw2003@gmail.com>
2025-07-10 00:29:52 -0400

    Made coin flip displays for regular + until flips

--- Code/Scenes/UI/coin.gd
+++ Code/Scenes/UI/coin.gd
@@ -2,10 +2,11 @@
 extends Control
 
 @export var coin_heads: CompressedTexture2D = load("res://Art/Coins/BreloomFirst.png")
-@export var offset: Vector2 = Vector2(0,-24)
+@export var offset: Vector2 = Vector2(0,-12)
 
 @onready var flip_anim: AnimatedSprite2D = %FlipAnim
 @onready var heads: TextureRect = %Heads
+@onready var animation_player: AnimationPlayer = %AnimationPlayer
 
 var result: bool
 
@@ -23,11 +24,15 @@ func play_flip():
 	toss()
 
 func toss():
-	var flip_tween: Tween = create_tween().set_ease(Tween.EASE_OUT)
-	position += offset
-	flip_tween.tween_property(self, "position", position - Vector2(0,11), .35)
+	var flip_tween: Tween = create_tween().\
+	set_ease(Tween.EASE_OUT).set_speed_scale(animation_player.speed_scale)
+	
+	flip_tween.tween_property(flip_anim, "position", flip_anim.position - Vector2(0,32), .35)
 	flip_tween.set_ease(Tween.EASE_IN)
-	flip_tween.tween_property(self, "position", position + Vector2(0,11), .15)
+	flip_tween.tween_property(flip_anim, "position", flip_anim.position, .15)
 
 func emit_result():
 	shown.emit()
+
+func reset():
+	%AnimationPlayer.play("RESET")
--- Code/Scenes/UI/until_box.gd
+++ Code/Scenes/UI/until_box.gd
@@ -3,9 +3,10 @@ extends Control
 
 @export var coinflip: CoinFlip
 @export var coin_scene: PackedScene
-@export var max_size: Vector2 = Vector2(152, 64)
+@export var base_cooldown: float = 1
+@export var growth_rate: float = 20
+@export var speedup: Curve
 
-@onready var coin_container: HBoxContainer = %CoinContainer
 @onready var top: PanelContainer = $VBoxContainer/Footer
 @onready var bottom: PanelContainer = $VBoxContainer/Footer2
 @onready var cooldown: Timer = $Cooldown
@@ -16,20 +17,16 @@ var shown_results: Dictionary[bool, int] = {true: 0, false: 0}
 
 func _ready() -> void:
 	#For testing coinflips will be done here
-	flip_results = coinflip.get_flip_array(coinflip.activate_CF())
-	flip_results.shuffle()
+	#flip_results = coinflip.get_flip_array(coinflip.activate_CF())
+	#debug results for extreme test
+	flip_results.resize(50)
+	flip_results.fill(true)
+	flip_results.append(false)
 	#on implementation the result should be found beforehand
 	
 	top.setup("[center]Coinflips")
 	bottom.setup(str("[center]Heads: ", shown_results[true],
 	 " Tails: ", shown_results[false]))
-	
-	for result in flip_results:
-		var new_coin = coin_scene.instantiate()
-		new_coin.result = result
-		new_coin.shown.connect(display_results.bind(result))
-		new_coin.add_to_group("coins")
-		coin_container.add_child(new_coin)
 
 func display_results(result: bool):
 	shown_results[result] += 1
@@ -37,17 +34,19 @@ func display_results(result: bool):
 	 " Tails: ", shown_results[false]))
 
 func _on_begin_timeout() -> void:
-	var coins: Array[Node] = get_tree().get_nodes_in_group("coins")
-	for i in range(coins.size()):
-		var coin: Node = coins[i]
-		coin.play_result()
+	print("SHOWING: ", flip_results)
+	for i in range(flip_results.size()):
+		var curve_pos: float = float(i+1) / growth_rate
+		
+		%Coin.animation_player.speed_scale = 1 * (speedup.sample(curve_pos) * 10)
+		%Coin.result = flip_results[i]
+		%Coin.play_result()
+		await %Coin.shown
+		
+		cooldown.wait_time = base_cooldown * (1 - speedup.sample(curve_pos))
 		cooldown.start()
-		if i > 3 and coins.size() > 4:
-			var scroll_tween: Tween = create_tween()
-			scroll_tween.tween_property(%CoinScroll, "scroll_horizontal", 
-			%CoinScroll.scroll_horizontal + 36, cooldown.wait_time)
 		await cooldown.timeout
-
-func _on_coin_container_resized() -> void:
-	print(%CoinContainer.size)
-	%CoinScroll.custom_minimum_size = clamp(%CoinContainer.size, Vector2.ZERO, max_size)
+		
+		%Coin.heads.hide()
+		%Coin.reset()
+		display_results(flip_results[i])
